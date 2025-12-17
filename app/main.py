a9b92d22343a0e152b80ea209887a48a5c9f9e39
ramzeth <ramzeth25@gmail.com>
2025-12-17 16:08:13 +0200

    Solution_1

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cached_data = 0
     parameters = []
-    def wrapper(*args, **kwargs):
-        nonlocal cached_data, parameters
+
+    def wrapper(*args, **kwargs) -> Any:
+        nonlocal cached_data
         if args and args not in parameters:
             parameters.append(args)
             cached_data = func(*args, **kwargs)
@@ -19,24 +20,4 @@ def cache(func: Callable) -> Callable:
         else:
             print("Getting from cache")
             return cached_data
-
     return wrapper
-
-
-
-@cache
-def long_time_func(a: int, b: int, c: int) -> int:
-    return (a ** b ** c) % (a * c)
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> int:
-    return [number ** power for number in n_tuple]
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
-
-
